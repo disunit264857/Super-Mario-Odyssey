f2ed5e6d487b52e7678f1b5b2ded0e06160e2851
disunit264857 <78911247+disunit264857@users.noreply.github.com>
2021-02-13 10:56:51 +0000

    Add files via upload

---
+++ java/AdditionalHealthMeter.java
@@ -0,0 +1,120 @@
+package com.disunit.supermarioodyssey.ui;
+
+import android.graphics.*;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+
+
+public class AdditionalHealthMeter extends UI {
+
+  public int health; // 0 to 3
+  private int healthB;
+  private int bw, bh;
+  private Bitmap texture;
+  private Bitmap heart;
+  private Canvas canvas;
+  
+  private Paint hpPaint;
+  private Paint emptyPaint;
+  private Paint backgroundPaint;
+  private Paint separaterPaint;
+
+  private float[] hsv;
+  
+  private long startTime;
+
+private float gap = 2f;
+
+  public AdditionalHealthMeter(int w, int h) {
+    super(w,h);
+    bw = w;
+    bh = h;
+    healthB = 0;
+    health = 2;
+    texture = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+    canvas = new Canvas(texture);
+    hpPaint = new Paint();
+    hpPaint.setStyle(Paint.Style.STROKE);
+    hpPaint.setAntiAlias(true);
+    hpPaint.setStrokeWidth(bw*0.12f);
+    hpPaint.setARGB(255,51,255,255);
+    
+    emptyPaint = new Paint();
+    emptyPaint.setStyle(Paint.Style.STROKE);
+    emptyPaint.setAntiAlias(true);
+    emptyPaint.setStrokeWidth(bw*0.12f);
+    
+    emptyPaint.setColor(Color.WHITE);
+    
+    separaterPaint = new Paint();
+    separaterPaint.setStyle(Paint.Style.STROKE);
+    separaterPaint.setAntiAlias(true);
+    separaterPaint.setStrokeWidth(bw*0.02f);
+    separaterPaint.setARGB(50,0,0,0);
+    separaterPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+    backgroundPaint = new Paint();
+    
+    backgroundPaint.setARGB(255,50,50,50);
+    backgroundPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OVER));
+    
+    
+    hsv = new float[]{0,1,1};
+    startTime = 0;
+    
+    
+    
+    
+    heart = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+    Canvas tmp = new Canvas(heart);
+    Path hPath = new Path();
+    hPath.moveTo(w*0.5f,h*0.25f);
+    hPath.quadTo(w*0.35f,h*0.35f,w*0.3f,h*0.45f);
+    
+    hPath.lineTo(w*0.7f,h*0.45f);
+    
+    hPath.quadTo(w*0.65f,h*0.35f,w*0.5f,h*0.25f);
+    hPath.close();
+    Paint hPaint = new Paint();
+    hPaint.setAntiAlias(true);
+    hPaint.setColor(Color.WHITE);
+    tmp.drawPath(hPath,hPaint);
+    tmp.drawCircle(w*(0.5f-0.1f),h*(0.45f+0.1f),w*0.1f*1.4f,hPaint);
+    tmp.drawCircle(w*(0.5f+0.1f),h*(0.45f+0.1f),w*0.1f*1.4f,hPaint);
+    
+  }
+  
+
+  public void setHealth() {
+    
+    startTime = System.currentTimeMillis();
+  }
+
+  public void render() {
+    canvas.drawColor(Color.TRANSPARENT,PorterDuff.Mode.CLEAR);
+  long delta;
+  float clampedDelta;
+  if((delta=System.currentTimeMillis()-startTime)<=600f){
+  clampedDelta = (float)Math.min(delta,500);
+  hsv[0] = 60*(healthB+(health-healthB)*(float)clampedDelta/500f)-60;
+  float angle = 120*(healthB+(health-healthB)*clampedDelta/500f);
+    
+    
+    canvas.drawArc(bw*0.1f,bh*0.1f,bw*0.9f,bh*0.9f,90,angle,false,hpPaint);
+    canvas.drawArc(bw*0.1f,bh*0.1f,bw*0.9f,bh*0.9f,90+angle,360-angle,false,emptyPaint);
+    
+    canvas.drawLine(bw*0.5f,bh*0.5f,bw*0.5f,bh,separaterPaint);
+    canvas.drawLine(bw*0.5f,bh*0.5f,bw*(0.5f+(float)Math.cos(210f/180*Math.PI)),bh*(0.5f+(float)Math.sin(210f/180*Math.PI)),separaterPaint);
+    
+    
+    canvas.drawLine(bw*0.5f,bh*0.5f,bw*(0.5f+(float)Math.cos(330f/180*Math.PI)),bh*(0.5f+(float)Math.sin(330f/180*Math.PI)),separaterPaint);
+    canvas.drawCircle(bw*0.5f,bh*0.5f,bw*0.46f,backgroundPaint);
+    
+    canvas.drawBitmap(heart,null,new RectF(0,0,bw,bh),null);
+    
+    material.updateTextureData(texture);
+    material.isUpToDate = false;
+    }
+  }
+}
+
